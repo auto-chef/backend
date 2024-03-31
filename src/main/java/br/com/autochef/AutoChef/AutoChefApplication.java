9611f04e2024dc34001fc671877b1b8d53466fa5
VikitorXD <rm551886@fiap.com.br>
2024-03-31 00:11:59 -0300

    First Commit

---
+++ src/main/java/br/com/autochef/AutoChef/AutoChefApplication.java
@@ -0,0 +1,13 @@
+package br.com.autochef.AutoChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/autochef/AutoChef/AutoChefApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autochef.AutoChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
