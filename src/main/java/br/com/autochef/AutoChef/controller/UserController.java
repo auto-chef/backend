f0537d1607277023ddb9d341663b48df0ec6646e
ericknathan <erick.capito@hotmail.com>
2024-04-05 00:57:45 -0300

    refactor: rename usuario to user controller

--- src/main/java/br/com/autochef/AutoChef/controller/UserController.java
+++ src/main/java/br/com/autochef/AutoChef/controller/UserController.java
@@ -17,56 +17,50 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.List;
 
 @RestController
-@RequestMapping("users")//localhost:8080/useres
-public class UsuarioController {
-
+@RequestMapping("users")
+public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    //Médoto para cadastrar
     @PostMapping
     @Transactional
-    public ResponseEntity<RegisterUserDto> post(@RequestBody RegisterUserDto registerUserDto,
-                                                UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<RegisterUserDto> post(@RequestBody RegisterUserDto registerUserDto, UriComponentsBuilder uriBuilder) {
         var user = new UserModel(registerUserDto);
         userRepository.save(user);
+
         var uri = uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(new RegisterUserDto(user));
     }
 
-    //Método para atualização - Name, Email, Password
     @PutMapping("{id}")
     @Transactional
-    public ResponseEntity<UpdateUserDto> put(@PathVariable("id")Long id,
-                                             @RequestBody UpdateUserDto udp){
+    public ResponseEntity<UpdateUserDto> put(@PathVariable("id")Long id, @RequestBody UpdateUserDto udp){
         var user = userRepository.getReferenceById(id);
         user.update(udp);
+
         return ResponseEntity.ok(new UpdateUserDto(user));
     }
 
-    //Método para deletar um cadastro
     @DeleteMapping("{id}")
     @Transactional
     public ResponseEntity<Void> delete(@PathVariable("id") Long id){
         userRepository.deleteById(id);
+
         return ResponseEntity.noContent().build();
     }
 
-    //Método que vai retornar um usuario pelo seu id
     @GetMapping("{id}")
     public ResponseEntity<DetailsUserDto> getby(@PathVariable("id") Long id){
         var user = userRepository.getReferenceById(id);
+
         return ResponseEntity.ok(new DetailsUserDto(user));
     }
 
-    //Método que retorna todos os usuarios cadastrados ordenado por id
     @GetMapping
     public ResponseEntity<List<RegisterUserDto>> get(Pageable pageable){
-        Pageable pg = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
-                Sort.by("id").ascending());
-        var listaUserDto = userRepository.findAll(pg).stream().
-                map(RegisterUserDto::new).toList();
-        return ResponseEntity.ok(listaUserDto);
-    }
+        Pageable pg = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("id").ascending());
 
+        var userList = userRepository.findAll(pg).stream().map(RegisterUserDto::new).toList();
+        return ResponseEntity.ok(userList);
+    }
 }
