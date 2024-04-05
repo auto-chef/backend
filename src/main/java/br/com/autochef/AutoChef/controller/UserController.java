fe5f9064c4da4496e6ffa6b69ec0d356fdcea523
VikitorXD <rm551886@fiap.com.br>
2024-04-05 00:57:07 -0300

    Criação produto

--- src/main/java/br/com/autochef/AutoChef/controller/UserController.java
+++ src/main/java/br/com/autochef/AutoChef/controller/UserController.java
@@ -22,10 +22,6 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-<<<<<<< HEAD:src/main/java/br/com/autochef/AutoChef/controller/UsuarioController.java
-    //Method for registering a new user
-=======
->>>>>>> 66f4696a3d9abd7e24118e2875ef909fde764edd:src/main/java/br/com/autochef/AutoChef/controller/UserController.java
     @PostMapping
     @Transactional
     public ResponseEntity<RegisterUserDto> post(@RequestBody RegisterUserDto registerUserDto, UriComponentsBuilder uriBuilder) {
@@ -36,10 +32,6 @@ public class UserController {
         return ResponseEntity.created(uri).body(new RegisterUserDto(user));
     }
 
-<<<<<<< HEAD:src/main/java/br/com/autochef/AutoChef/controller/UsuarioController.java
-    //Method for updating a user's registration
-=======
->>>>>>> 66f4696a3d9abd7e24118e2875ef909fde764edd:src/main/java/br/com/autochef/AutoChef/controller/UserController.java
     @PutMapping("{id}")
     @Transactional
     public ResponseEntity<UpdateUserDto> put(@PathVariable("id")Long id, @RequestBody UpdateUserDto udp){
@@ -49,10 +41,6 @@ public class UserController {
         return ResponseEntity.ok(new UpdateUserDto(user));
     }
 
-<<<<<<< HEAD:src/main/java/br/com/autochef/AutoChef/controller/UsuarioController.java
-    //Method to delete a user's registration
-=======
->>>>>>> 66f4696a3d9abd7e24118e2875ef909fde764edd:src/main/java/br/com/autochef/AutoChef/controller/UserController.java
     @DeleteMapping("{id}")
     @Transactional
     public ResponseEntity<Void> delete(@PathVariable("id") Long id){
@@ -61,11 +49,6 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
-<<<<<<< HEAD:src/main/java/br/com/autochef/AutoChef/controller/UsuarioController.java
-    //
-    //Method to search for a user by ID
-=======
->>>>>>> 66f4696a3d9abd7e24118e2875ef909fde764edd:src/main/java/br/com/autochef/AutoChef/controller/UserController.java
     @GetMapping("{id}")
     public ResponseEntity<DetailsUserDto> getby(@PathVariable("id") Long id){
         var user = userRepository.getReferenceById(id);
@@ -73,10 +56,6 @@ public class UserController {
         return ResponseEntity.ok(new DetailsUserDto(user));
     }
 
-<<<<<<< HEAD:src/main/java/br/com/autochef/AutoChef/controller/UsuarioController.java
-    //Method to search all users
-=======
->>>>>>> 66f4696a3d9abd7e24118e2875ef909fde764edd:src/main/java/br/com/autochef/AutoChef/controller/UserController.java
     @GetMapping
     public ResponseEntity<List<RegisterUserDto>> get(Pageable pageable){
         Pageable pg = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("id").ascending());
