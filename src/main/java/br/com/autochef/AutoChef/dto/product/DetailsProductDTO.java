fecf0a892e8976588f195d285d0b554add6de981
ericknathan <erick.capito@hotmail.com>
2024-05-05 18:56:50 -0300

    feat: add product id to product details dto

--- src/main/java/br/com/autochef/AutoChef/dto/product/DetailsProductDTO.java
+++ src/main/java/br/com/autochef/AutoChef/dto/product/DetailsProductDTO.java
@@ -5,8 +5,8 @@ import br.com.autochef.AutoChef.model.ProductModel;
 
 import java.math.BigDecimal;
 
-public record DetailsProductDTO(Long id, String imageUrl, String name, BigDecimal price, ProductCategory category) {
-    public DetailsProductDTO(ProductModel product){
-        this(product.getId(), product.getImageUrl(), product.getName(), product.getPrice(), product.getProductCategory());
+public record DetailsProductDTO(Long id, String imageUrl, String name, BigDecimal price, ProductCategory category, Long restaurantId) {
+    public DetailsProductDTO(ProductModel product) {
+        this(product.getId(), product.getImageUrl(), product.getName(), product.getPrice(), product.getProductCategory(), product.getRestaurant().getId());
     }
 }
