2965e90dbad0d8a8f7140ffb0baece6700b3720e
VikitorXD <rm551886@fiap.com.br>
2024-05-05 16:46:40 -0300

    Refector: Add validation from User and Order DTO

--- src/main/java/br/com/autochef/AutoChef/dto/order/UpdateOrderDTO.java
+++ src/main/java/br/com/autochef/AutoChef/dto/order/UpdateOrderDTO.java
@@ -1,9 +1,14 @@
 package br.com.autochef.AutoChef.dto.order;
 
 import br.com.autochef.AutoChef.enums.OrderStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UpdateOrderDTO(
         OrderStatus status,
+        @NotBlank @Size(max=355)
         String feedbackDescription,
+        @Size(min=0, max=5)
         Integer feedbackRate) {
 }
--- src/main/java/br/com/autochef/AutoChef/dto/user/RegisterUserDTO.java
+++ src/main/java/br/com/autochef/AutoChef/dto/user/RegisterUserDTO.java
@@ -8,13 +8,13 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
 public record RegisterUserDTO(
-        @Size(max=255)
+        @NotBlank @Size(max=255)
         String name,
-        @Size(min=11,max=11)
+        @NotBlank @Size(min=11,max=11)
         String cpf,
-        @Size(max=255)
+        @NotBlank @Size(max=255)
         String email,
-        @Size(max=255)
+        @NotBlank @Size(max=255)
         String password,
         @Future @NotNull
         LocalDate birthDate) {
